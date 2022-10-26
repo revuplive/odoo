0aa7c965c06973f1a74a180fb1cdf8826462ef42
Rahul Mehra <rmehra9292@gmail.com>
2022-10-26 19:30:47 +0530

    cart fixed

--- custom-addons/loonwholesale/static/src/js/product.js
+++ custom-addons/loonwholesale/static/src/js/product.js
@@ -22,10 +22,43 @@ odoo.define('loonwholesale.loon_product', function (require) {
             this._super.apply(this, arguments);
         },
 
-        _addToCart: function(ev) {
+        callserver: function (url, data, callback) {
+            // ev.preventDefault();
+            ajax.post({
+                method: 'POST',
+                url: url, 
+                data: data,
+                ContentType: 'application/json'
+            }).then(function (result) {
+                callback(result)
+            }).guardedCatch(function (error) {
+                // resolve();
+            });
+        },
+
+        _addToCart: function (ev) {
             var $item = $(ev.currentTarget);
             var $form = $item.parent().parent().find('form');
             var $input = $item.parent().parent().find('input.addtc');
+            var token = $form.find('.oe_product_image')[0].childNodes[1].value;
+            var data = {
+                'product_id': 1,
+                'add_qty': $input.val(),
+                'csrf_token': token
+            }
+            console.log(data)
+            // this.callserver('/shop/cart/update_json', data, null);
+            return this._rpc({
+                route: "/shop/cart/update_json",
+                params: data,
+            }).then(async data => {
+                console.log(data, '++++++++++++====')
+                // if (data.cart_quantity && (data.cart_quantity !== parseInt($(".my_cart_quantity").text()))) {
+                //     await animateClone($('header .o_wsale_my_cart').first(), this.$itemImgContainer, 25, 40);
+                //     updateCartNavBar(data);
+                // }
+            });
+            $item.innerHtml = 'Added'
         }
     })
 });
